b8453fa3f9185eb0d2a9997f253c812b74c70b18
yule-li <liyule@buaa.edu.cn>
2018-05-08 14:33:22 +0800

    timeline

--- work_togother/tf_face/test.sh
+++ work_togother/tf_face/test.sh
@@ -1,2 +1,3 @@
-MODEL_DIR=models/model-20180309-083949.ckpt-60000
-CUDA_VISIBLE_DEVICES=1 python test/test.py dataset/lfw-112X96 ${MODEL_DIR} --lfw_file_ext jpg --network_type sphere_network
+#MODEL_DIR=models/model-20180309-083949.ckpt-60000
+MODEL_DIR=models/_sphere_network_softmax_112_1/model-softmax.ckpt-60000
+CUDA_VISIBLE_DEVICES=0 python test/test.py dataset/lfw-112X96 ${MODEL_DIR} --lfw_file_ext jpg --network_type sphere_network
